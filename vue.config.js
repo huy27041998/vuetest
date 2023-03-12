dbbd02b71cb785ec087523d14856df3960a0d187
Huy <quanghuyle9@gmail.com>
2023-03-13 01:19:20 +0700

    init

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/test_github_page/'
+      : '/'
 })
